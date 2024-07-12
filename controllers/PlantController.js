f44fe194e1ac390e192a6bebc16046cf08de4a55
welferdinand <welferdinand@gmail.com>
2024-07-12 18:52:08 +0700

    Feat Pest and Plant Controller

--- controllers/PlantController.js
+++ controllers/PlantController.js
@@ -1,5 +1,32 @@
-class PlantController {
+const {Plant, Category} = require('../models')
 
+class PlantController {
+    static async getAllPlants(req,res,next){
+        try {
+            let plants = await Plant.findAll({
+                include: Category
+            })
+            if(!plants) throw {name:"NotFound"}
+
+            res.status(200).json(plants)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async getPlantById(req,res,next){
+        try {
+            let {id} = req.params
+
+            let plant = await Plant.findByPk(id, {
+                include: Category
+            })
+
+            res.status(200).json(plant)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports = PlantController
