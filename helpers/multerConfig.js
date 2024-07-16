e8c0334458e946aaa7ec3968614e852474c6f982
JundiHN <jundihn@gmail.com>
2024-07-16 18:21:26 +0700

    feat: error

--- helpers/multerConfig.js
+++ helpers/multerConfig.js
@@ -3,12 +3,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dlq01v0tu",
-  api_key: "935517527171615",
-  api_secret: "zDt298ee1ZRrwQdS9_SGxDVOnS8",
-  //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  //   api_key: process.env.CLOUDINARY_API_KEY,
-  //   api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
