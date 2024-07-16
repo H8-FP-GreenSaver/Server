5d88df4b25a96436d941ac490c99dbeae0992319
JundiHN <jundihn@gmail.com>
2024-07-16 18:13:17 +0700

    feat: impelementasi cloudinary for post threads

---
+++ helpers/multerConfig.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dlq01v0tu",
+  api_key: "935517527171615",
+  api_secret: "zDt298ee1ZRrwQdS9_SGxDVOnS8",
+  //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  //   api_key: process.env.CLOUDINARY_API_KEY,
+  //   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads",
+    allowed_formats: ["jpg", "jpeg", "png"],
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
--- routers/index.js
+++ routers/index.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const router = express.Router()
-const userRouter = require('./user')
-const plantRouter = require('./plant')
-const pestRouter = require('./pest')
-const stepRouter = require('./step')
-const ErrorHandler = require('../middlewares/ErrorHandler')
-const authentication = require('../middlewares/authentication')
+const express = require("express");
+const router = express.Router();
+const userRouter = require("./user");
+const plantRouter = require("./plant");
+const pestRouter = require("./pest");
+const stepRouter = require("./step");
+const uploadRouter = require("./upload");
+const ErrorHandler = require("../middlewares/ErrorHandler");
+const authentication = require("../middlewares/authentication");
 
-router.use('/users', userRouter)
+router.use("/users", userRouter);
 
-router.use(authentication)
-router.use('/plants', plantRouter)
-router.use('/pests', pestRouter)
-router.use('/steps', stepRouter)
+router.use(authentication);
+router.use("/forums", uploadRouter);
+router.use("/plants", plantRouter);
+router.use("/pests", pestRouter);
+router.use("/steps", stepRouter);
 
-router.use(ErrorHandler)
+router.use(ErrorHandler);
 
-module.exports = router
+module.exports = router;
